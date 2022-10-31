ed68b247e089a76d098ddb8db9a2bc380b93a044
Maxime Braud <85800785+KodiakFR@users.noreply.github.com>
2022-10-31 16:55:56 +0100

    création de la page MoodForm

---
+++ lib/screens/moodFormPage/mood_form_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:my_mood/screens/widget/navbar.dart';
+
+class MoodFormPage extends StatelessWidget {
+  const MoodFormPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const NavBar(),
+      appBar: AppBar(
+        title: const Text('My Mood'),
+      ),
+      body: Center(child: Column(
+        children: [
+          
+        ],
+      )),
+    );
+  }
+}
