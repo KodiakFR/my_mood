d9c1c40be5fa11e5d11ff6258765da4203dceaf1
Maxime Braud <85800785+KodiakFR@users.noreply.github.com>
2023-01-13 18:51:59 +0100

    Mise en place de firbase local + online

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,73 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_mood/screens/homepage/home_page.dart';
 import 'package:my_mood/screens/moodFormPage/mood_form_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'screens/moodFormPage/wiriting_form_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //si je suis en mode debeug
+  if (kDebugMode) {
+    // création d'un host pour la plateform Android ou IOS
+    final String host = Platform.isAndroid ? "10.0.2.2" : "localhost";
+
+    //Permet la connection à l'émulateur en local
+    await FirebaseAuth.instance.useAuthEmulator(
+      host,
+      9099,
+    );
+
+    FirebaseFirestore.instance.useFirestoreEmulator(
+      host,
+      8080,
+    );
+
+    FirebaseFunctions.instance.useFunctionsEmulator(
+      host,
+      9199,
+    );
+
+    FirebaseFunctions.instance.useFunctionsEmulator(
+      host,
+      5001,
+    );
+  }
+  //permet d'évité du cache dans le FirebaseFirestore
+  await FirebaseFirestore.instance.terminate();
+  await FirebaseFirestore.instance.clearPersistence();
+
+  runApp(MyApp(
+    firebaseAuth: FirebaseAuth.instance,
+    firebaseFirestore: FirebaseFirestore.instance,
+    firebaseStorage: FirebaseStorage.instance,
+    firebaseFunctions: FirebaseFunctions.instance,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FirebaseAuth firebaseAuth;
+  final FirebaseFirestore firebaseFirestore;
+  final FirebaseStorage firebaseStorage;
+  final FirebaseFunctions firebaseFunctions;
+
+  const MyApp(
+      {super.key,
+      required this.firebaseAuth,
+      required this.firebaseFirestore,
+      required this.firebaseFunctions,
+      required this.firebaseStorage});
 
   // This widget is the root of your application.
   @override
@@ -18,8 +76,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-          "/MoodForm":(context) => MoodFormPage(),
-          "/WriteForm":(context) => WritingFormPage(),
+        "/MoodForm": (context) => MoodFormPage(),
+        "/WriteForm": (context) => WritingFormPage(),
       },
       title: 'My Mood',
       theme: ThemeData(
@@ -29,4 +87,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
