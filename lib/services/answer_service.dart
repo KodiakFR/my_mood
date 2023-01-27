c1c68d98e2373a4d2b0bc29d550bcbd0efbaf429
dimitri <dig72000@gmail.com>
2023-01-27 19:37:25 +0100

    modification of pages and adding bdd request (insert answer)

--- lib/services/answer_service.dart
+++ lib/services/answer_service.dart
@@ -4,17 +4,29 @@ import 'package:my_mood/models/answer_entity.dart';
 
 class AnswerService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  
   Future addAnswerFromUser(AnswerEntity answerEntity) async {
+        DocumentSnapshot<Map<String, dynamic>> result = 
+              await FirebaseFirestore.instance.collection('users').doc(_auth.currentUser!.uid).get();       
     answerEntity.dateTime = DateTime.now();
     try {
-        await FirebaseFirestore.instance.
-        collection('users').doc(_auth.currentUser!.uid).update({
-          'weatherMood' : answerEntity.weather,
-          'moodType' : answerEntity.moodTypeChoose,
-          'writingMood' : answerEntity.writingMood,
-          'date' : answerEntity.dateTime
+        if(result.data()?["date"] == null) {
+                await FirebaseFirestore.instance.collection('users').doc(_auth.currentUser!.uid).set({
+          'weatherMood' : FieldValue.arrayUnion([answerEntity.weather.toString()]),
+          'moodType' :  FieldValue.arrayUnion([answerEntity.moodTypeChoose.toString()]),
+          'writingMood' : FieldValue.arrayUnion([answerEntity.writingMood.toString()]),
+          'date' : FieldValue.arrayUnion([answerEntity.dateTime.toString()])
         });
+        } else {
+          print("dans le else");
+                    await FirebaseFirestore.instance.collection('users').doc(_auth.currentUser!.uid).update({
+          'weatherMood' : FieldValue.arrayUnion([answerEntity.weather.toString()]),
+          'moodType' :  FieldValue.arrayUnion([answerEntity.moodTypeChoose.toString()]),
+          'writingMood' : FieldValue.arrayUnion([answerEntity.writingMood.toString()]),
+          'date' : FieldValue.arrayUnion([answerEntity.dateTime.toString()])
+        });
+        }
+    
     } catch(e) {
       print(e.toString());
     }
